8c028979000d12ec7847adb6550c592c3eeb04bf
Pankaj Kumar2 <pankaj.kumar2@insight.com>
2026-09-24 11:22:31 +0530

    refactor: moved terraform code to dedicated travelbooking-infra repository

--- terraform/artifact-registry.tf
+++
@@ -1,7 +0,0 @@
-resource "google_artifact_registry_repository" "docker" {
-  location      = var.region
-  repository_id = "travelbooking"
-  description   = "Docker images for TravelBooking microservices"
-  format        = "DOCKER"
-  project       = var.project_id
-}
